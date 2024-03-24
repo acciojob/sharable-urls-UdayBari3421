ac87839aeea53944849d8ae9ad2850c4d4c70760
Uday Bari <udaybari2003@gmail.com>
2024-03-24 21:51:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// your code here
+let form = document.querySelector("form");
+
+let str = "https://localhost:8080/"
+form.addEventListener("submit",(e)=>{
+	e.preventDefault();
+	document.querySelector("#url").innerText=`${str}?name=${form.name.value}&year${form.year.value}`;
+	form.reset();
+});

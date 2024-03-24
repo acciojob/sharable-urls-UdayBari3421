2f4de7c576fe587968b3721f90d73b3ead67b78d
Uday Bari <udaybari2003@gmail.com>
2024-03-24 21:56:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let form = document.querySelector("form");
 let str = "https://localhost:8080/"
 form.addEventListener("submit",(e)=>{
 	e.preventDefault();
-	document.querySelector("#url").innerText=`${str}?name=${form.name.value}&year${form.year.value}`;
+	if (form.name.value && !form.year.value) {
+		document.querySelector("#url").innerText=`${str}?name=${form.name.value}`;
+	} else if (!form.name.value && form.year.value) {
+		document.querySelector("#url").innerText=`${str}?year=${form.year.value}`;
+	} else{
+		document.querySelector("#url").innerText=`${str}?name=${form.name.value}&year${form.year.value}`;
+	}
 	form.reset();
 });
